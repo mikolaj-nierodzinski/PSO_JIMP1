cbd618106b088319c161cb5986c8e1a81e3fa0c5
baophuccqt <hnbp3011@gmail.com>
2026-01-29 17:11:15 +0100

    fix psoo

--- src/logger.c
+++ src/logger.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include "logger.h"
 
-void loggerLog(int idx) {
-    printf("logged something here...%d\n", idx);
+void loggerLog(int partId, double x, double y, FILE *log) {
+    fprintf(log, "Particle no.; %d; ", partId);
+    fprintf(log, " %g; %g;\n", x, y);
+}
+
+void loggerSep(int iter, FILE *log){
+	fprintf(log, "iteration no.; %d; x coordinate; y coordinate; \n", iter);
 }
